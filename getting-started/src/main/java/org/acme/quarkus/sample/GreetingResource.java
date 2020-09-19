bdb30eef202a2301331166a2a1fdeaa54a01eb9b
2015bcs506 <2015bcs506@sggs.ac.in>
2020-09-19 12:10:45 +0000

    commiting with usage of MEDIATYPE application_json and @headerparam and @Queryparam

--- getting-started/src/main/java/org/acme/quarkus/sample/GreetingResource.java
+++ getting-started/src/main/java/org/acme/quarkus/sample/GreetingResource.java
@@ -1,16 +1,33 @@
 package org.acme.quarkus.sample;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
 
+    public static final String TOKEN="dev";
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
     }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String addHello(@HeaderParam("token")String hName, @QueryParam("token") String aName){
+        String token=hName!=null?hName:aName;
+        if (!TOKEN.equals(token)){
+            throw new  RuntimeException("Unauthorized");
+        }
+        return "{\"key\":\""+token+"\"}";
+    }
+
 }
