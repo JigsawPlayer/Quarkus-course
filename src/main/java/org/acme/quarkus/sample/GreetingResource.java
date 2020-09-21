d57f42e532b49ea53f706abfb2903a8d922cbdcc
JigsawPlayer <kulkarniravindra@outlook.com>
2020-09-21 19:30:31 +0000

    Updating code with Dependency Injection

--- src/main/java/org/acme/quarkus/sample/GreetingResource.java
+++ src/main/java/org/acme/quarkus/sample/GreetingResource.java
@@ -2,6 +2,7 @@ package org.acme.quarkus.sample;
 
 import java.util.UUID;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -11,22 +12,20 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import org.acme.quarkus.sample.service.GreetingService;
+
 
 @Path("/hello")
 public class GreetingResource {
 
-    public static final String TOKEN="dev";
+    @Inject
+    private GreetingService greetingService;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(@QueryParam("name")String name ) {
         
-        return String.format("Hello %s,your id is %s", name, UUID.randomUUID().toString());
+        return greetingService.sayHello(name);
     }
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello( ) {
-        return("Hello");
-        //return String.format("Hello %s,your id is %s", UUID.randomUUID().toString(), UUID.randomUUID().toString());
-    }
 }
