b3db7c2f13f212baf1dcecb60ef40150c19f5781
JigsawPlayer <kulkarniravindra@outlook.com>
2020-09-22 19:10:49 +0000

    Commiting changes with Functional tests

--- src/main/java/org/acme/quarkus/sample/GreetingResource.java
+++ src/main/java/org/acme/quarkus/sample/GreetingResource.java
@@ -1,17 +1,12 @@
 package org.acme.quarkus.sample;
 
-import java.util.UUID;
-
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
 import org.acme.quarkus.sample.service.GreetingService;
 
 
@@ -22,10 +17,17 @@ public class GreetingResource {
     private GreetingService greetingService;
 
     @GET
+    @Path("/{name}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello(@QueryParam("name")String name ) {
+    public String hello(@PathParam("name")String name ) {
         
         return greetingService.sayHello(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return greetingService.sayHello();
     }
 
 }
--- src/main/java/org/acme/quarkus/sample/service/GreetingService.java
+++ src/main/java/org/acme/quarkus/sample/service/GreetingService.java
@@ -6,6 +6,10 @@ import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class GreetingService {
+    public String sayHello(){
+        return("hello");
+    }
+
     public String sayHello(String name){
         return String.format("Hello %s,your id is %s", name, UUID.randomUUID().toString());
     }
